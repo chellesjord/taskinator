2643aabaffce25b80e5afb812a4674bdb3a6ee0f
chellesjord <m.kayejordan@gmail.com>
2021-11-18 18:01:23 -0600

    save tasks to loacal storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,8 +60,7 @@ var createTaskEl = function (taskDataObj) {
     // increase task counter for next unique id
     taskIdCounter++;
 
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
+    saveTasks();
 };
 
 var createTaskActions = function (taskId) {
@@ -119,6 +118,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
         }
     };
 
+    saveTasks();
+
     alert("Task Updated!");
 
     // remove data attribute from form
@@ -167,7 +168,7 @@ var taskStatusChangeHandler = function (event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+    saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -213,8 +214,12 @@ var deleteTask = function (taskId) {
 
     //reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
-
+    saveTasks();
 };
+
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
